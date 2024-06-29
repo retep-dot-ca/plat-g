e491b76885be3941172720f44980ef37659c04b9
retep-dot-ca <84478012+retep-dot-ca@users.noreply.github.com>
2024-06-29 16:23:56 -0500

    Update vboot.sh

--- vboot.sh
+++ vboot.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
+#Victim
 
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>>log.out 2>&1
+
+SETUPCOMPLETE="/var/log/setupcomplete.log"
+if [[ ! -f $SETUPCOMPLETE ]]; then
+   #FirstRun
+   
+   #the next line creates an empty file so it won't run the next boot
+   touch "$SETUPCOMPLETE"
+
+else
+   echo "Second Run"
+
+fi
