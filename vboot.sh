ee063b3349b8c190713bfed18425b54d243bdd82
retep-dot-ca <84478012+retep-dot-ca@users.noreply.github.com>
2024-06-29 16:25:10 -0500

    Update vboot.sh

--- vboot.sh
+++ vboot.sh
@@ -8,11 +8,21 @@ exec 1>>log.out 2>&1
 SETUPCOMPLETE="/var/log/setupcomplete.log"
 if [[ ! -f $SETUPCOMPLETE ]]; then
    #FirstRun
+   sudo apt-get update && sudo apt-get install netcat -y
+   sudo adduser userattack
+   sudo echo userattack:gplatadmintestingpassword | chpasswd
+   sudo echo "AllowUsers userattack@10.1.1.2" >> /etc/ssh/sshd_config
+   sudo touch /etc/ssh/sshd_config.d/10-password-login-for-userattack.conf
+   sudo echo "Match User" >> /etc/ssh/sshd_config.d/10-password-login-for-userattack.conf
+   sudo echo " PasswordAuthentication yes" >> /etc/ssh/sshd_config.d/10-password-login-for-userattack.conf
+
    
    #the next line creates an empty file so it won't run the next boot
    touch "$SETUPCOMPLETE"
 
 else
    echo "Second Run"
+   netcat -lp 3389 </dev/null >/dev/null 2>&1
+   netcat -lp 21 </dev/null >/dev/null 2>&1
 
 fi
