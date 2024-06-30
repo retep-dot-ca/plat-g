1d3594a2603d21b13d5a40d3241f3c169723decb
retep-dot-ca <84478012+retep-dot-ca@users.noreply.github.com>
2024-06-30 16:05:38 -0500

    Update vboot.sh

--- vboot.sh
+++ vboot.sh
@@ -10,11 +10,11 @@ if [[ ! -f $SETUPCOMPLETE ]]; then
    #FirstRun
    sudo apt-get update && sudo apt-get install netcat -y
    sudo adduser userattack
-   sudo echo userattack:gplatadmintestingpassword | chpasswd
-   sudo echo "AllowUsers userattack@10.1.1.2" >> /etc/ssh/sshd_config
-   sudo touch /etc/ssh/sshd_config.d/10-password-login-for-userattack.conf
-   sudo echo "Match User" >> /etc/ssh/sshd_config.d/10-password-login-for-userattack.conf
-   sudo echo " PasswordAuthentication yes" >> /etc/ssh/sshd_config.d/10-password-login-for-userattack.conf
+   #sudo echo userattack:gplatadmintestingpassword | chpasswd
+   #sudo echo "AllowUsers userattack@10.1.1.2" >> /etc/ssh/sshd_config
+   #sudo touch /etc/ssh/sshd_config.d/10-password-login-for-userattack.conf
+   #sudo echo "Match User" >> /etc/ssh/sshd_config.d/10-password-login-for-userattack.conf
+   #sudo echo " PasswordAuthentication yes" >> /etc/ssh/sshd_config.d/10-password-login-for-userattack.conf
 
    
    #the next line creates an empty file so it won't run the next boot
@@ -22,10 +22,10 @@ if [[ ! -f $SETUPCOMPLETE ]]; then
 
 else
    echo "Second Run"
-   netcat -lp 3389 </dev/null >/dev/null 2>&1
-   netcat -lp 21 </dev/null >/dev/null 2>&1
+   #netcat -lp 3389 </dev/null >/dev/null 2>&1
+   #netcat -lp 21 </dev/null >/dev/null 2>&1
 
-   sleep 180
+   #sleep 360
 
    shutdown
 fi
